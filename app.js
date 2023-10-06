3f5ec5fcd9f96cf45aff02f4ec582560db2b47e5
Kriti <61482773+kas140472@users.noreply.github.com>
2023-10-06 11:36:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,3 +17,14 @@
     animateButtonClick(lcBtn);
     window.location.href = "projects.html";
   });
+
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
